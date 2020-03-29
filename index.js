6040ed9163680b8a37718e0f96b2c32863939bb6
soseger <katjasofiaseger@gmail.com>
2020-03-29 17:28:45 +0300

    send whatsapp message when product is available

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+require('dotenv').config();
+
 const fetch = require('node-fetch');
+const twilio = require('twilio');
+
+const client = new twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 const productIds = ['146183292', '146183254'];
 const url = `https://hullutpaivat.com/api/balance?orinIds=${productIds.join(
   ','
 )}`;
+
+async function notify(productId) {
+  try {
+    await client.messages.create({
+      body: `Tuoleja on tullut varastoon! https://hullutpaivat.com/tuote/${productId}`,
+      to: `whatsapp:${process.env.NUMBER_TO}`,
+      from: `whatsapp:${process.env.NUMBER_FROM}`
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 (async function run() {
   try {
@@ -12,7 +32,7 @@ const url = `https://hullutpaivat.com/api/balance?orinIds=${productIds.join(
 
     Object.keys(json.balances).forEach(key => {
       if (json.balances[key]) {
-        console.log('Löytyy varastosta');
+        notify(key);
       }
     });
   } catch (e) {
